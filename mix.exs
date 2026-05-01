7653db5994442226361bceb494cb9c44ed97ca62
Raul Baez <rbaez@bancatlan.sv>
2026-05-01 18:42:50 -0500

    Upgrade version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Hexagen.MixProject do
   def project do
     [
       app: :hexagen,
-      version: "0.1.4",
+      version: "0.1.5",
       elixir: "~> 1.15",
       description:
         "The Architecture Guardian for Phoenix. A strict Hexagonal Architecture scaffolding tool.",
